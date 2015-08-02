4664da6d530ae50758e96848a29fd85d7db2701a
Rohit Sehgal <rsehgal@cse.iitk.ac.in>
2015-08-02 23:38:54 +0530

    Update Create_CFG.java

--- Create_CFG.java
+++ Create_CFG.java
@@ -5,23 +5,29 @@ import soot.SootClass;
 import soot.Body;
 import soot.SootMethod;
 import soot.toolkits.graph.*;
-
+import java.util.Iterator;
 
 public class Do_CFG {
 	
 	public static void main(String[] args) {
-		
+	 
 		SootClass sc =  Scene.v().loadClassAndSupport("cfgAnalysis.Demo1");
 		sc.setApplicationClass();
 		
-		 
 		SootMethod sm = sc.getMethodByName("perform_computation");
 		Body b = sm.retrieveActiveBody();
 		
 		BriefUnitGraph ug = new BriefUnitGraph(b);
 		BriefBlockGraph bg = new BriefBlockGraph(ug);
 		
-		System.out.println(bg.getBlocks());
+		Iterator<Block> i = bg.iterator();
+		
+		while(i.hasNext()){
+			Block blk = (Block)i.next();
+			int n = blk.getIndexInMethod();
+			System.out.println(n);
+			//System.out.println(i.next());
+		}
+		//System.out.println(bg.getBlocks());
 	}
-
 }
